54ff7bd4983e2ebdced89d9169a719aa796c3235
juliashenn <julias12@illinois.edu>
2026-05-02 22:43:26 -0500

    small weapon changes

--- HiddenFoe/Assets/Scripts/Player.cs
+++ HiddenFoe/Assets/Scripts/Player.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public Canvas endScreen;
+    // public Canvas endScreen;
 
-    void Start()
-    {
-        if (endScreen != null) 
-        {
-            endScreen.SetActive(false);
-        }
-    }
+    // void Start()
+    // {
+    //     if (endScreen != null) 
+    //     {
+    //         endScreen.SetActive(false);
+    //     }
+    // }
 
-    void Update()
-    {
+    // void Update()
+    // {
         
-    }
+    // }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("LethalObject"))
-        {
-            // LevelManager.EndGame();
-            if (endScreen != null) 
-            {
-                endScreen.SetActive(true);
-            }
-        }
-    }
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("LethalObject"))
+    //     {
+    //         // LevelManager.EndGame();
+    //         if (endScreen != null) 
+    //         {
+    //             endScreen.SetActive(true);
+    //         }
+    //     }
+    // }
 }
